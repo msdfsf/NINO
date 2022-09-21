58d3c2f98e19ce7662510b55ac53016cd5604755
msdfsf <86263024+msdfsf@users.noreply.github.com>
2022-09-21 17:09:37 +0200

    Create PluginState.h

---
+++ src/program/PluginState.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace PluginState {
+	
+	enum PluginState {
+
+		OFF,
+		// BEGIN [keep this in same order] 
+		ON,
+		ON_LEFT,
+		ON_RIGHT,
+		// END
+		TO_BE_DELETED,
+		DELETED
+
+	};
+
+}
