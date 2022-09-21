6db94acae144dbb9098d9a21226eefc886058eac
msdfsf <86263024+msdfsf@users.noreply.github.com>
2022-09-21 17:13:08 +0200

    Update System.cpp

--- src/program/System.cpp
+++ src/program/System.cpp
@@ -6,7 +6,12 @@
 #include <windows.h>
 #include "winuser.h"
 
+// drawing stuff
+#pragma comment(lib, "Msimg32.lib")
+
 namespace System {
+
+	void* windowHandler = NULL;
 
 	const HCURSOR defaultCursor = LoadCursorW(0, IDC_ARROW);
 	const HCURSOR pointerCursor = LoadCursorW(0, IDC_HAND);
@@ -24,6 +29,76 @@ namespace System {
 				break;
 
 		}
+
+	}
+
+	void setWindowSize(int width, int height) {
+		
+		RECT winRect;
+		GetWindowRect((HWND) windowHandler, &winRect);
+
+		winRect.right = winRect.left + width;
+		winRect.bottom = winRect.top + height;
+
+		AdjustWindowRectEx(&winRect, GetWindowLongA((HWND)windowHandler, GWL_STYLE), 0, WS_EX_CLIENTEDGE);
+
+		SetWindowPos(
+			(HWND)windowHandler,
+			NULL,
+			winRect.left,
+			winRect.top,
+			winRect.right - winRect.left,
+			winRect.bottom - winRect.top,
+			NULL
+		);
+	
+	}
+	
+	void blockResize() {
+		
+		LONG style = GetWindowLongA((HWND)windowHandler, GWL_STYLE);
+		SetWindowLongA(
+			(HWND) windowHandler,
+			GWL_STYLE,
+			style &~ WS_SIZEBOX
+		);
+
+	}
+
+	void allowResize() {
+
+		LONG style = GetWindowLongA((HWND)windowHandler, GWL_STYLE);
+		SetWindowLongA(
+			(HWND)windowHandler,
+			GWL_STYLE,
+			style | WS_SIZEBOX
+		);
+
+	}
+	
+	void blockMaximize() {
+
+		LONG style = GetWindowLongA((HWND)windowHandler, GWL_STYLE);
+		SetWindowLongA(
+			(HWND)windowHandler,
+			GWL_STYLE,
+			style &~ WS_MAXIMIZEBOX
+		);
+
+	}
+	
+	void allowMaximize() {
+		
+		LONG style = GetWindowLong((HWND) windowHandler, GWL_STYLE);
+		SetWindowLong(
+			(HWND)windowHandler,
+			GWL_STYLE,
+			style | WS_MAXIMIZEBOX
+		);
+
+	}
+
+	void drawPixels() {
 
 	}
 
