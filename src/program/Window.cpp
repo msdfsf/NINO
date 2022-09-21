ca03900774e8ce8fdd1983a793e1999c63b42b54
msdfsf <86263024+msdfsf@users.noreply.github.com>
2022-09-21 17:18:05 +0200

    Update Window.cpp

--- src/program/Window.cpp
+++ src/program/Window.cpp
@@ -1,10 +1,56 @@
 #pragma once
 
+#include <vector>
+
 #include "Window.h"
 #include "Cursor.h"
+#include "CallbackEvent.h"
+
+typedef struct Callback {
+
+	Control* target;
+	void (*fcn) (Control* source, CTRL_PARAM paramA, CTRL_PARAM paramB);
+
+
+} Callback;
+
+std::vector<Callback*> callbacks;
+
+void Window::addEventCallback(int eventType, void (*fcn) (Control* source, CTRL_PARAM paramA, CTRL_PARAM paramB), Control* source) {
+	
+	switch (eventType) {
+
+		case CallbackEvent::MOUSE_CLICK: {
+
+			Callback* cb = (Callback*)malloc(sizeof(Callback));
+			if (!cb) return;
+
+			cb->target = (source) ? source : Control::window;
+			cb->fcn = fcn;
+			callbacks.push_back(cb);
+			
+			break;
+		
+		}
+	
+	}
+
+}
+
+void windowMouseClickCallback(Control* source, CTRL_PARAM paramA, CTRL_PARAM paramB) {
+
+	for (int i = 0; i < callbacks.size(); i++) {
+		const Callback cb = *callbacks[i];
+		cb.fcn(cb.target, paramA, paramB);
+	}
+
+}
 
 Window::Window() : Control() {
 
+	this->eMouseClick = &windowMouseClickCallback;
+
+	this->resizeType = ResizeType::SCALE;
 	this->cursor = Cursor::DEFAULT;
 
 }
